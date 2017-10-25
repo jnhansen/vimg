b85cb0fdab01b10baa4fe9e1329e116d07716662
Johannes Hansen <johannes.niklas.hansen@gmail.com>
2017-10-25 15:35:35 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ def readme():
         return f.read()
 
 setup(name='vimg',
-      version='0.2.2',
+      version='0.3.0',
       description='An image viewer for the command line',
       long_description=readme(),
+      keywords='cli terminal console image picture graphics viewer preview',
       url='http://github.com/jnhansen/vimg',
       author='Johannes Hansen',
       author_email='johannes.niklas.hansen@gmail.com',
