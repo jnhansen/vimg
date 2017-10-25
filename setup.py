d5801b9be1f157457f6ce20c8fc047cccea05e56
Johannes Hansen <johannes.niklas.hansen@gmail.com>
2017-10-25 16:37:18 +0100

    added classifiers

--- setup.py
+++ setup.py
@@ -13,6 +13,18 @@ setup(name='vimg',
       author='Johannes Hansen',
       author_email='johannes.niklas.hansen@gmail.com',
       license='MIT',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: Viewers',
+        'Topic :: Utilities'
+      ],
       packages=find_packages(),
       install_requires=[
         'numpy',
